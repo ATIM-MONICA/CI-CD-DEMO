22b903d0cab0825b6081bc5241cc8a16b3572f58
ATIM-MONICA <monicadeann.ja@gmail.com>
2025-04-18 10:01:42 +0300

    Add tests to work

--- greeting/tests.py
+++ greeting/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, client
+from django.urls import reverse
 
-# Create your tests here.
+# Create your tests here.pip
+class GreetingTests(TestCase):
+    def setUp(self):
+        self.client = client()
+        
+    def text_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Hello')
+        self.assertTemplateUsed(response, 'index.html')
+        
+class GreetingFunctionalityTests(TestCase):
+    def test_greeting(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.context['greeting'], 'Hello')
+        
